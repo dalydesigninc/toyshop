672d2a9d5cce339379b724f26f3c2d64b94935a1
Errroron <79564992+Errroron@users.noreply.github.com>
2025-12-01 03:59:40 -0800

    deploy update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://toyshopautorepair.com',
+  site: 'https://dalydesigninc.github.io',
+  base: '/toyshop',
 })
